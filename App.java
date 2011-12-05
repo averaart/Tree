2a3b9edcc353bfac897c501fe6cfab2b26ca5aa1
Youri <mail@tjang.nl>
2011-12-05 21:34:02 +0100

    Opdracht met Studentenboom

---
+++ App.java
@@ -0,0 +1,25 @@
+
+public class App {
+//	In deze opdracht programmeer je een Binaire boom.
+//	Dit moet je doen door zelf de link-structuur te 
+//	implementeren. In de boom worden studenten opgeslagen.
+//	De knopen in de boom hebben als key het studentnummer 
+//	van de studenten.
+	
+	
+	/**
+	 * De main wordt gebruikt om de tree te testen.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+//		Programmeer een paar cases waarbij 
+//		je laat zien dat de boom werkt
+		
+//		voeg studenten met studentnummers Ó0802063Ó tot en met Ó0824836Ó toe
+		
+//		Verwijder de root
+		
+//		print de studenten op volgorde van studentnummer
+	}
+
+}
---
+++ Tree.java
@@ -0,0 +1,57 @@
+
+public class Tree {
+
+	private class Node {
+	
+	}
+
+	//---------------------------
+	// Constructors
+	public Tree(){
+		
+	}
+	
+	
+	//--------------------------
+	//Data structure operations	
+	/**
+	 * Voeg een element toe aan de tree
+	 */
+	public void insert(Object o){
+		
+	}
+	
+	/**
+	 * Verwijder een object uit de boom
+	 */
+	public void remove(/*Studentnummer*/){
+		
+	}
+	
+	
+	/**
+	 * Zoek een studentnummer in de boom en return 
+	 * het oject
+	 */
+	public Object find(/*Studentnummer*/){
+		return new Object();
+	}
+	
+	public boolean isBalanced(){
+		return false;//dummy
+	}
+	
+	public void balance(){
+		
+	}
+	
+	
+	//------------------------------------------------------
+	// Utility methods
+	/**
+	 * Print de tree In-Order
+	 */
+	public String toString(){
+		return "";
+	}	
+}
